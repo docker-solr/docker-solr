2cd35c6a3322e38a2d32664032637ffaae79fe87
Martijn Koster <mak-github@greenhills.co.uk>
2016-04-08 22:38:27 +0100

    Refactor build-all.sh
    Reduce repetition
    Change the cwd to the build directory in preparation for "COPY" in Dockerfiles.
    I assume the official build process also does this, but I don't know for sure.

--- build-all.sh
+++ build-all.sh
@@ -6,36 +6,38 @@
 set -e
 
 TAG_BASE=docker-solr/docker-solr
+
 # Override with e.g.: export SOLR_DOWNLOAD_SERVER=http://www-eu.apache.org/dist/lucene/solr
 SOLR_DOWNLOAD_SERVER=${SOLR_DOWNLOAD_SERVER:-'http://www-us.apache.org/dist/lucene/solr'}
 
 cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
 
+function build {
+  local version=$1
+  local variant=$2
+
+  if [[ -z $variant ]]; then
+    build_dir="$version"
+    docker_tag=$TAG_BASE:$version
+  else
+    build_dir="$version/$variant"
+    docker_tag=$TAG_BASE:$version-$variant
+  fi
+  echo "BUILDING in $build_dir for $docker_tag"
+  (cd $build_dir
+   fullVersion="$(grep -m1 'ENV SOLR_VERSION' Dockerfile | cut -d' ' -f3)"
+   docker build --pull --rm=true --tag="$docker_tag" --build-arg SOLR_DOWNLOAD_SERVER=$SOLR_DOWNLOAD_SERVER .
+  )
+  echo "tagged $docker_tag"
+}
+
 buildable=$(ls | grep -E '^[0-9]+\.[0-9]+$' | sort --version-sort)
 latest=$(echo "$buildable" | tail -n 1)
 for version in $buildable ; do
-  dockerfile=$version/Dockerfile
-  dockertag=$TAG_BASE:$version
-  if [ ! -e $dockerfile ]; then
-    echo "No $dockerfiles; skipping"
-  else
-    echo "BUILDING $dockerfile"
-    fullVersion="$(grep -m1 'ENV SOLR_VERSION' "$dockerfile" | cut -d' ' -f3)"
-    echo "Building: docker build --pull --rm=true --tag="$dockertag" - < $dockerfile"
-    docker build --pull --rm=true --tag="$dockertag" --build-arg SOLR_DOWNLOAD_SERVER=$SOLR_DOWNLOAD_SERVER - < $dockerfile
+    build $version
+    build $version 'alpine'
     if [ "$version" = "$latest" ]; then
-      docker tag "$dockertag" "$TAG_BASE:latest"
+        docker tag "$TAG_BASE:$version" "$TAG_BASE:latest"
+        echo "tagged $TAG_BASE:latest"
     fi
-  fi
-
-  alpine_dockerfile=$version/alpine/Dockerfile
-  alpine_dockertag=$TAG_BASE:$version-alpine
-  if [ ! -e $alpine_dockerfile ]; then
-    echo "No $alpine_dockerfile; skipping"
-  else
-    echo "BUILDING $alpine_dockerfile"
-    fullVersion="$(grep -m1 'ENV SOLR_VERSION' "$alpine_dockerfile" | cut -d' ' -f3)"
-    echo "Building: docker build --pull --rm=true --tag="$alpine_dockertag" - < $alpine_dockerfile"
-    docker build --pull --rm=true --tag="$alpine_dockertag" --build-arg SOLR_DOWNLOAD_SERVER=$SOLR_DOWNLOAD_SERVER - < $alpine_dockerfile
-  fi
 done
