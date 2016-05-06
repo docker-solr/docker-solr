904d84fcaa83082d8a1b7a93a980928700387ea5
Martijn Koster <mak-github@greenhills.co.uk>
2016-05-06 12:34:44 +0100

    update other wait-for-solr.sh scripts

--- 6.0/scripts/wait-for-solr.sh
+++ 6.0/scripts/wait-for-solr.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 # A helper script to wait for solr
-until $(wget -q -O - http://localhost:8983 | grep -q -i solr); do
+until wget -q -O - http://localhost:8983 | grep -q -i solr; do
   echo "solr is not running yet"
   sleep 5
 done
