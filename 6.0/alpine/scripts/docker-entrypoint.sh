c0423fc2e94f943b6e9fc09049f2de7d0b2a3baa
Martijn Koster <mak-github@greenhills.co.uk>
2016-06-08 19:36:55 +0100

    apply typo fix to all files

--- 6.0/alpine/scripts/docker-entrypoint.sh
+++ 6.0/alpine/scripts/docker-entrypoint.sh
@@ -87,7 +87,7 @@ elif [[ "$1" = 'solr-precreate' ]]; then
     # To simply create a core:
     #      docker run -P -d solr solr-precreate mycore
     # To create a core from mounted config:
-    #      docker run -P -d -c $PWD/myconfig:/myconfig solr solr-precreate mycore /myconfig
+    #      docker run -P -d -v $PWD/myconfig:/myconfig solr solr-precreate mycore /myconfig
     # To create a core in a mounted directory:
     #      mkdir mycores; chown 8983:8983
     #      docker run -it --rm -P -v $PWD/mycores:/opt/solr/server/solr/mycores solr solr-precreate mycore
