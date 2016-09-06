d4200613a308c1b11b6b6d114e6129bdc4c8b9d4
Martijn Koster <mak-github@greenhills.co.uk>
2016-09-06 15:08:58 +0100

    fix indentation

--- update.sh
+++ update.sh
@@ -14,8 +14,8 @@ GPG_KEYSERVER=${GPG_KEYSERVER:-hkp://pool.sks-keyservers.net}
 
 versions=( "$@" )
 if [ ${#versions[@]} -eq 0 ]; then
-	echo "Usage: bash update.sh [version ...]"
-	exit 1
+    echo "Usage: bash update.sh [version ...]"
+    exit 1
 fi
 versions=( "${versions[@]%/}" )
 
@@ -52,75 +52,75 @@ curl -sSL $archiveUrl | sed -r -e 's,.*<a href="(([0-9])+\.([0-9])+\.([0-9])+)/"
 
 mkdir -p $DOWNLOADS
 for version in "${versions[@]}"; do
-	full_version="$(grep "^$version" "$upstream_versions" | tail -n 1)"
-	if [[ -z $full_version ]]; then
-		echo "Cannot find $version in $archiveUrl"
-		exit 1
-	fi
-	(
-		set -x
+    full_version="$(grep "^$version" "$upstream_versions" | tail -n 1)"
+    if [[ -z $full_version ]]; then
+        echo "Cannot find $version in $archiveUrl"
+        exit 1
+    fi
+    (
+        set -x
 
-                cd $DOWNLOADS
+        cd $DOWNLOADS
 
-		# get the tgz, so we can checksum it, and verify the signature
-		output=solr-$full_version.tgz
-		partial_url=$full_version/solr-$full_version.tgz
-		if [ ! -f solr-$full_version.tgz ]; then
-			if ! wget -nv --output-document=$output $archiveUrl/$partial_url; then
-				echo "Could not fetch $archiveUrl/$partial_url"
-				exit 1
-			fi
-		fi
+        # get the tgz, so we can checksum it, and verify the signature
+        output=solr-$full_version.tgz
+        partial_url=$full_version/solr-$full_version.tgz
+        if [ ! -f solr-$full_version.tgz ]; then
+            if ! wget -nv --output-document=$output $archiveUrl/$partial_url; then
+                echo "Could not fetch $archiveUrl/$partial_url"
+                exit 1
+            fi
+        fi
 
-		# The Solr release process publish MD5 and SHA1 checksum files. Check those first so we get a clear
-		# early failure for incomplete downloads, and avoid scary-sounding PGP mismatches
-		if [ ! -f solr-$full_version.tgz.sha1 ]; then
-			wget -nv --output-document=solr-$full_version.tgz.sha1 $archiveUrl/$full_version/solr-$full_version.tgz.sha1
-		fi
-		sha1sum -c solr-$full_version.tgz.sha1
-		if [ ! -f solr-$full_version.tgz.md5 ]; then
-			wget -nv --output-document=solr-$full_version.tgz.md5 $archiveUrl/$full_version/solr-$full_version.tgz.md5
-		fi
-		md5sum -c solr-$full_version.tgz.md5
+        # The Solr release process publish MD5 and SHA1 checksum files. Check those first so we get a clear
+        # early failure for incomplete downloads, and avoid scary-sounding PGP mismatches
+        if [ ! -f solr-$full_version.tgz.sha1 ]; then
+            wget -nv --output-document=solr-$full_version.tgz.sha1 $archiveUrl/$full_version/solr-$full_version.tgz.sha1
+        fi
+        sha1sum -c solr-$full_version.tgz.sha1
+        if [ ! -f solr-$full_version.tgz.md5 ]; then
+            wget -nv --output-document=solr-$full_version.tgz.md5 $archiveUrl/$full_version/solr-$full_version.tgz.md5
+        fi
+        md5sum -c solr-$full_version.tgz.md5
 
-		# We will record a stronger SHA256
-		SHA256=$(sha256sum solr-$full_version.tgz | awk '{print $1}')
+        # We will record a stronger SHA256
+        SHA256=$(sha256sum solr-$full_version.tgz | awk '{print $1}')
 
-		# get the PGP signature
-		if [ ! -f solr-$full_version.tgz.asc ]; then
-			wget -nv --output-document=solr-$full_version.tgz.asc $archiveUrl/$full_version/solr-$full_version.tgz.asc
-		fi
+        # get the PGP signature
+        if [ ! -f solr-$full_version.tgz.asc ]; then
+            wget -nv --output-document=solr-$full_version.tgz.asc $archiveUrl/$full_version/solr-$full_version.tgz.asc
+        fi
 
-		# Get the code signing keys
-		# Per http://www.apache.org/dyn/closer.html and a message from Hoss on
-		# http://stackoverflow.com/questions/32539810/apache-lucene-5-3-0-release-keys-missing-key-3fcfdb3e
-		wget -nv --output-document KEYS $archiveUrl/$full_version/KEYS
-		gpg --import KEYS
+        # Get the code signing keys
+        # Per http://www.apache.org/dyn/closer.html and a message from Hoss on
+        # http://stackoverflow.com/questions/32539810/apache-lucene-5-3-0-release-keys-missing-key-3fcfdb3e
+        wget -nv --output-document KEYS $archiveUrl/$full_version/KEYS
+        gpg --import KEYS
 
-		# and for some extra verification we check the key on the keyserver too:
-		KEY=$(gpg --status-fd 1 --batch --verify solr-$full_version.tgz.asc solr-$full_version.tgz 2>&1 | awk '$1 == "[GNUPG:]" && ($2 == "BADSIG" || $2 == "VALIDSIG") { print $3; exit }')
-		gpg --keyserver "$GPG_KEYSERVER" --recv-key "$KEY" || {
-                    echo "Failed to get the key from the key server"
-                    exit 1
-                }
+        # and for some extra verification we check the key on the keyserver too:
+        KEY=$(gpg --status-fd 1 --batch --verify solr-$full_version.tgz.asc solr-$full_version.tgz 2>&1 | awk '$1 == "[GNUPG:]" && ($2 == "BADSIG" || $2 == "VALIDSIG") { print $3; exit }')
+        gpg --keyserver "$GPG_KEYSERVER" --recv-key "$KEY" || {
+            echo "Failed to get the key from the key server"
+            exit 1
+        }
 
-		# verify the signature matches our content
-		gpg --batch --verify solr-$full_version.tgz.asc solr-$full_version.tgz
-		# get the full fingerprint (since we only get the "long id" if it was BADSIG before)
-		KEY=$(gpg --status-fd 1 --batch --verify solr-$full_version.tgz.asc solr-$full_version.tgz 2>&1 | awk '$1 == "[GNUPG:]" && $2 == "VALIDSIG" { print $3; exit }')
+        # verify the signature matches our content
+        gpg --batch --verify solr-$full_version.tgz.asc solr-$full_version.tgz
+        # get the full fingerprint (since we only get the "long id" if it was BADSIG before)
+        KEY=$(gpg --status-fd 1 --batch --verify solr-$full_version.tgz.asc solr-$full_version.tgz 2>&1 | awk '$1 == "[GNUPG:]" && $2 == "VALIDSIG" { print $3; exit }')
 
-		if [ -z "$KEEP_ALL_ARTIFACTS" ]; then
-			rm solr-$full_version.tgz.asc solr-$full_version.tgz.sha1 solr-$full_version.tgz.md5
-			if [ -z "$KEEP_SOLR_ARTIFACT" ]; then
-				rm solr-$full_version.tgz
-			fi
-		fi
+        if [ -z "$KEEP_ALL_ARTIFACTS" ]; then
+            rm solr-$full_version.tgz.asc solr-$full_version.tgz.sha1 solr-$full_version.tgz.md5
+            if [ -z "$KEEP_SOLR_ARTIFACT" ]; then
+                rm solr-$full_version.tgz
+            fi
+        fi
 
-                cd ..
+        cd ..
 
-	        write_files $full_version
-	        write_files $full_version 'alpine'
-	)
+        write_files $full_version
+        write_files $full_version 'alpine'
+    )
 done
 
 if [ -f $DOWNLOADS/KEYS ]; then rm $DOWNLOADS/KEYS; fi
