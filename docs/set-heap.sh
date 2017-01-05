5a12e7321b61f2873447316df4a1316309e44f41
Martijn Koster <mak-github@greenhills.co.uk>
2017-01-05 14:39:14 +0000

    Add documentation to set-heap.sh

--- docs/set-heap.sh
+++ docs/set-heap.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+#
+# This script is mainly an illustration for the docker-entrypoint-initdb.d extension mechanism.
+# The SOLR_HEAP configuration technique here is usable for older versions of Solr.
+# From Solr 6.3 setting the SOLR_HEAP can be done more easily with:
+#
+#   docker run -d -P -e SOLR_HEAP=800m docker-solr/docker-solr:6.3.0
+#
 set -e
 cp /opt/solr/bin/solr.in.sh /opt/solr/bin/solr.in.sh.orig
 sed -e 's/SOLR_HEAP=".*"/SOLR_HEAP="1024m"/' </opt/solr/bin/solr.in.sh.orig >/opt/solr/bin/solr.in.sh
