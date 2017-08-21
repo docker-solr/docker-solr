6a42532e5080910a99572b8ccad1f16ab9e2230e
Martijn Koster <mak-github@greenhills.co.uk>
2017-08-21 12:52:07 +0100

    Update generate-stackbrew-library.sh
    Update generate-stackbrew-library.sh to the latest from
    https://github.com/docker-library/official-images/blob/master/library/elasticsearch
    which uses RFC 2822 format.
    Additional tweak to skip non-version directories.
    
    Add other architectures supported by
    https://github.com/docker-library/official-images/blob/master/library/openjdk
    
    For https://github.com/docker-solr/docker-solr/issues/128

--- generate-stackbrew-library.sh
+++ generate-stackbrew-library.sh
@@ -1,51 +1,101 @@
 #!/bin/bash
-set -e
+#
+# Based on https://github.com/docker-library/elasticsearch/blob/master/generate-stackbrew-library.sh
+set -eu
 
-declare -A aliases
-aliases=(
-        [6.6]='6 latest'
-        [5.5]='5'
+declare -A aliases=(
+    [6.6]='6 latest'
+    [5.5]='5'
 )
 
+self="$(basename "$BASH_SOURCE")"
 cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
+extglob_old=$(shopt -p extglob||true)
+shopt -s extglob
 
-versions=( */ )
+versions=( +([0-9])\.+([0-9])/ )
+eval "$extglob_old"
+
 versions=( "${versions[@]%/}" )
-url='git://github.com/docker-solr/docker-solr'
 
-echo '# maintainer: Martijn Koster <mak-github@greenhills.co.uk> (@makuk66)'
-echo '# maintainer: Shalin Mangar <shalin@apache.org> (@shalinmangar)'
+# sort version numbers with highest first
+IFS=$'\n'; versions=( $(echo "${versions[*]}" | sort -rV) ); unset IFS
+
+# get the most recent commit which modified any of "$@"
+fileCommit() {
+	git log -1 --format='format:%H' HEAD -- "$@"
+}
+
+# get the most recent commit which modified "$1/Dockerfile" or any file COPY'd from "$1/Dockerfile"
+dirCommit() {
+	local dir="$1"; shift
+	(
+		cd "$dir"
+		fileCommit \
+			Dockerfile \
+			$(git show HEAD:./Dockerfile | awk '
+				toupper($1) == "COPY" {
+					for (i = 2; i < NF; i++) {
+						print $i
+					}
+				}
+			')
+	)
+}
+
+cat <<-EOH
+# this file is generated via https://github.com/docker-library/solr/blob/$(fileCommit "$self")/$self
+
+Maintainers: Martijn Koster <mak-github@greenhills.co.uk> (@makuk66),
+             Shalin Mangar <shalin@apache.org> (@shalinmangar)
+GitRepo: https://github.com/docker-solr/docker-solr.git
+EOH
+
+# prints "$2$1$3$1...$N"
+join() {
+	local sep="$1"; shift
+	local out; printf -v out "${sep//%/%%}%s" "$@"
+	echo "${out#$sep}"
+}
 
 for version in "${versions[@]}"; do
-        if [ ! -f "$version/Dockerfile" ]; then
-          continue
-        fi
+	commit="$(dirCommit "$version")"
 
-	commit="$(git log -1 --format='format:%H' -- "$version")"
-	fullVersion="$(grep -m1 'ENV SOLR_VERSION' "$version/Dockerfile" | cut -d' ' -f3)"
-	
+	fullVersion="$(git show "$commit":"$version/Dockerfile" | awk '$1 == "ENV" && $2 == "ELASTICSEARCH_VERSION" { gsub(/~/, "-", $3); print $3; exit }')"
+
+	rcVersion="${version%-rc}"
+
 	versionAliases=()
-	while [ "$fullVersion" != "$version" -a "${fullVersion%[.-]*}" != "$fullVersion" ]; do
+	while [ "$fullVersion" != "$rcVersion" -a "${fullVersion%[.-]*}" != "$fullVersion" ]; do
 		versionAliases+=( $fullVersion )
 		fullVersion="${fullVersion%[.-]*}"
 	done
-	versionAliases+=( $version ${aliases[$version]} )
-	
+	versionAliases+=(
+		$rcVersion
+		${aliases[$version]:-}
+	)
+
 	echo
-	for va in "${versionAliases[@]}"; do
-		echo "$va: ${url}@${commit} $version"
-	done
+	cat <<-EOE
+		Tags: $(join ', ' "${versionAliases[@]}")
+		Architectures: amd64, arm32v5, arm32v7, arm64v8, i386, ppc64le, s390x
+		GitCommit: $commit
+		Directory: $version
+	EOE
 
 	for variant in alpine; do
-		commit="$(git log -1 --format='format:%H' -- "$version/$variant")"
+		[ -f "$version/$variant/Dockerfile" ] || continue
+
+		commit="$(dirCommit "$version/$variant")"
+
+		variantAliases=( "${versionAliases[@]/%/-$variant}" )
+		variantAliases=( "${variantAliases[@]//latest-/}" )
+
 		echo
-		for va in "${versionAliases[@]}"; do
-			if [ "$va" = 'latest' ]; then
-				va="$variant"
-			else
-				va="$va-$variant"
-			fi
-			echo "$va: ${url}@${commit} $version/$variant"
-		done
+		cat <<-EOE
+			Tags: $(join ', ' "${variantAliases[@]}")
+			GitCommit: $commit
+			Directory: $version/$variant
+		EOE
 	done
 done
