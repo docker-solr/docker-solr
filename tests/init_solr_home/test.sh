7b8db2693b8cd3415c7077187c8d8fa0a173f4a9
Martijn Koster <mak-github@greenhills.co.uk>
2017-09-05 11:05:27 +0100

    test case for INIT_SOLR_HOME

---
+++ tests/init_solr_home/test.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+#
+set -euo pipefail
+
+TEST_DIR="$(dirname -- "${BASH_SOURCE-$0}")"
+
+if (( $# == 0 )); then
+  echo "Usage: $BASH_SOURCE tag"
+  exit
+fi
+
+tag=$1
+
+if [[ ! -z "${DEBUG:-}" ]]; then
+  set -x
+fi
+
+source "$TEST_DIR/../shared.sh"
+
+echo "Test $tag"
+container_name='test_'$(echo "$tag" | tr ':/-' '_')
+echo "Cleaning up left-over containers from previous runs"
+container_cleanup "$container_name"
+echo "Running $container_name"
+if [ -d mysolrhome ]; then
+  docker run --rm -v "$PWD/mysolrhome:/mysolrhome" "$tag" bash -c "rm -fr /mysolrhome/*"
+  rm -fr mysolrhome
+fi
+
+mkdir mysolrhome
+chmod a+w mysolrhome
+docker run --name "$container_name" -d -v "$PWD/mysolrhome:/mysolrhome" -e SOLR_HOME=/mysolrhome -e INIT_SOLR_HOME=yes -d "$tag" "solr-demo"
+SLEEP_SECS=5
+echo "Sleeping $SLEEP_SECS seconds..."
+sleep $SLEEP_SECS
+container_status=$(docker inspect --format='{{.State.Status}}' "$container_name")
+echo "container $container_name status: $container_status"
+if [[ $container_status == 'exited' ]]; then
+  docker logs "$container_name"
+  exit 1
+fi
+
+SLEEP_SECS=10
+echo "Sleeping $SLEEP_SECS seconds..."
+sleep $SLEEP_SECS
+echo "Checking Solr is running"
+status=$(docker exec "$container_name" /opt/docker-solr/scripts/wait-for-solr.sh)
+if ! egrep -q 'solr is running' <<<$status; then
+  echo "Test test_simple $tag failed; solr did not start"
+  container_cleanup "$container_name"
+  exit 1
+fi
+sleep 5
+echo "Checking data"
+data=$(docker exec --user=solr "$container_name" wget -q -O - 'http://localhost:8983/solr/demo/select?q=address_s%3ARound Rock')
+if ! egrep -q 'One Dell Way Round Rock, Texas 78682' <<<$data; then
+  echo "Test test_simple $tag failed; data did not load"
+  exit 1
+fi
+container_cleanup "$container_name"
+
+docker run --rm -v "$PWD/mysolrhome:/mysolrhome" "$tag" bash -c "rm -fr /mysolrhome/*"
+rmdir mysolrhome
+
+echo "Test $BASH_SOURCE $tag succeeded"
