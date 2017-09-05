055ff82217a57fc93a8cc9e5f4894e4ab8da1e69
Martijn Koster <mak-github@greenhills.co.uk>
2017-09-05 11:04:44 +0100

    fix container cleanup in tests

--- tests/shared.sh
+++ tests/shared.sh
@@ -6,7 +6,7 @@ TAG_LOCAL_BASE=docker-solr/docker-solr
 
 function container_cleanup {
   local container_name=$1
-  previous=$(docker ps --filter name="$container_name" --format '{{.ID}}' --no-trunc)
+  previous=$(docker ps --all --filter name="$container_name" --format '{{.ID}}' --no-trunc)
   if [[ ! -z $previous ]]; then
     echo "killing $container_name"
     docker kill "$container_name" || true
