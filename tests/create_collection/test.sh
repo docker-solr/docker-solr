3a66bae7c4c3039184f9503e2a3607d95c5bff5e
Martijn Koster <mak-github@greenhills.co.uk>
2018-09-21 17:36:24 +0100

    add test case for creating a collection

---
+++ tests/create_collection/test.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+#
+set -euo pipefail
+
+TEST_DIR="$(dirname -- $(readlink -f "${BASH_SOURCE-$0}"))"
+
+if (( $# == 0 )); then
+  echo "Usage: $BASH_SOURCE tag"
+  exit
+fi
+
+tag=$1
+
+if [[ ! -z "${DEBUG:-}" ]]; then
+  set -x
+fi
+
+source "$TEST_DIR/../shared.sh"
+
+echo "Test $TEST_DIR $tag"
+container_name='test_'$(echo "$tag" | tr ':/-' '_')
+echo "Cleaning up left-over containers from previous runs"
+container_cleanup "$container_name"
+echo "Running $container_name"
+docker run --name "$container_name" -d "$tag" solr-create -cloud -c mycollection
+
+wait_for_server_started "$container_name"
+
+echo "Checking cloud mode"
+docker exec --user=solr "$container_name" wget -q -O - http://localhost:8983/solr/admin/info/system | \
+  grep -q '"mode":"solrcloud",'
+
+echo "Loading data"
+docker exec --user=solr "$container_name" bin/post -c mycollection example/exampledocs/manufacturers.xml
+sleep 1
+echo "Checking data"
+data=$(docker exec --user=solr "$container_name" wget -q -O - 'http://localhost:8983/solr/mycollection/select?q=id%3Adell')
+if ! egrep -q 'One Dell Way Round Rock, Texas 78682' <<<$data; then
+  echo "Test $TEST_DIR $tag failed; data did not load"
+  exit 1
+fi
+container_cleanup "$container_name"
+
+echo "Test $TEST_DIR $tag succeeded"
