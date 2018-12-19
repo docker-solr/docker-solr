b5dc6d472751faead6a8422d3344c2edcb92d81f
Martijn Koster <mak-github@greenhills.co.uk>
2018-12-19 11:02:41 +0000

    various builder changes
    keep separate builder_tools dir
    move bash logic from Dockerfile into scripts
    separate gosu builder
    stop printing the solr version
    remove deprecated maintainer, use labels instead

---
+++ builder_tools/get-gpg-keys.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -euo pipefail
+
+for key in "${@}"; do 
+    found=''
+    for server in \
+      ha.pool.sks-keyservers.net \
+      hkp://keyserver.ubuntu.com:80 \
+      hkp://p80.pool.sks-keyservers.net:80 \
+      pgp.mit.edu \
+    ; do 
+      echo "  trying $server for $key"
+      gpg --batch --keyserver "$server" --keyserver-options timeout=10 --recv-keys "$key" && found=yes && break
+    done
+    test -z "$found" && echo >&2 "error: failed to fetch $key from several disparate servers -- network issues?" && exit 1; 
+done
+exit 0
+
