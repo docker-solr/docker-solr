d4409122404908caa03ab1a0a4e3edf27433649e
Martijn Koster <mak-github@greenhills.co.uk>
2018-12-20 13:25:23 +0000

    multistage build
    Use multistage builds in the Dockerfiles, so we can avoid
    build-specific things like gpg ending up in the image.
    
    Refactor some bash code inlines in the Dockerfiles into
    scripts in a separate builder_tools dir, so we can just
    copy that to the builder images and run it. This makes
    it easier to maintain, we can run shellceck, and share
    the scripts between Dockerfiles.
    
    Move the gosu fetching/checking into a separate builder.
    This improves caching during development
    
    No longer try to pull the base image prior to the docker build,
    because the multiple FROMs have made that too complicated.
    
    Stop printing the solr version, to further simplify the Dockerfile
    that previously rewrote that. If we want that, we can do the
    rewriting in update.sh
    
    Remove deprecated MAINTAINER tag from the Dockerfiles,
    use labels instead.
    
    Move some ENVs to ARGs to reduce environmental pollution.

--- tools/build_test_push.sh
+++ tools/build_test_push.sh
@@ -21,10 +21,6 @@ fi
 build_dir="$(readlink -f "$build_dir")"
 relative_dir="$(sed "s,$TOP_DIR/,," <<< "$build_dir")"
 
-parent="$(grep '^FROM' "$build_dir/Dockerfile" | sed -E 's/^.*FROM *//')"
-echo "pulling $parent"
-docker pull "$parent" >/dev/null 2>&1
-
 "$TOP_DIR/tools/build.sh" "$build_dir"
 full_version=$(awk --field-separator ':' '$1 == "'"$relative_dir"'" {print $2}' "$TOP_DIR/TAGS")
 "$TOP_DIR/tests/test.sh" "$full_version"
