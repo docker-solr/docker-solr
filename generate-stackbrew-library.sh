4e1e9472f2426202d7fa7706ce272a4cbeb669c4
Martijn Koster <mak-github@greenhills.co.uk>
2019-01-07 13:06:54 +0000

    Revert "Update generate-stackbrew-library.sh for multibuild"
    This reverts commit 3647e528b83949f2f3c3f9cb6fcb446510df97b7.

--- generate-stackbrew-library.sh
+++ generate-stackbrew-library.sh
@@ -107,7 +107,7 @@ for version in "${versions[@]}"; do
              fi
 		fi
 
-		variantParent="$(awk 'toupper($1) == "FROM" { print $2 }' "$dir/Dockerfile" | head -n 1)"
+		variantParent="$(awk 'toupper($1) == "FROM" { print $2 }' "$dir/Dockerfile")"
 		variantArches="${parentRepoToArches[$variantParent]}"
 
 		echo
