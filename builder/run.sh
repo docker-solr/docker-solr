065514c0594b5b1a50e459b479b88efdc5dff83c
Martijn Koster <mak-github@greenhills.co.uk>
2019-02-11 22:16:03 +0000

    Revert "run builder as user" There was a reason for that: we need root for docker
    This reverts commit b94d49f34a8b8a8f191528c6d53949d124389229.

--- builder/run.sh
+++ builder/run.sh
@@ -5,5 +5,9 @@ cd "$TOP_DIR"
 docker run -it \
   -v /var/run/docker.sock:/var/run/docker.sock \
   -v "$PWD:$PWD" \
-  --user "$(id -u):$(id -g)" \
+  --user "root" \
   -w "$PWD" dockersolr/builder bash "$@"
+if find "$PWD" -user root >/dev/null 2>&1; then
+  echo "chowning $PWD"
+  sudo chown -R "$(id -u):$(id -g)" "$PWD"
+fi
