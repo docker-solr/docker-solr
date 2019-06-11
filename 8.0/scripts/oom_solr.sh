366f3c2df2e4aab62888126fb006a6d3a1e85315
Peter Philipp <peter.philipp@cando-image.com>
2019-06-11 14:36:11 +1200

    Adjust Dockerfile templates and update all Dockerfiles using the update script. Changes: - Overhauled scripts as of feedback in https://github.com/docker-solr/docker-solr/pull/219 - Dedicated oom_solr.sh scripts for the two solr version ranges (<8, >=8). - Use tini installation approach documented in official docker images: https://github.com/docker-library/official-images/blob/master/README.md#init

--- 8.0/scripts/oom_solr.sh
+++ 8.0/scripts/oom_solr.sh
@@ -3,7 +3,6 @@
 # Custom oom handler but widely based on
 # https://github.com/apache/lucene-solr/blob/master/solr/bin/oom_solr.sh
 
-SOLR_LOGS_DIR="${MY_SOLR_LOGS_DIR:-/opt/solr/server/logs}"
 SOLR_PID=$(ps auxww | grep start.jar | grep -v grep | awk '{print $2}')
 if [[ -z "$SOLR_PID" ]]; then
   echo "Couldn't find Solr process running!"
