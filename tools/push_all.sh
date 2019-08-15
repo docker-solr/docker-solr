b8b0d37166c41edfe562671b4be47b1e4c6ada08
Martijn Koster <mak-github@greenhills.co.uk>
2019-08-15 07:35:08 +0100

    shellcheck

--- tools/push_all.sh
+++ tools/push_all.sh
@@ -15,7 +15,7 @@ for full_version_variant in $full_version_variants; do
   readarray -t tags < <(awk --field-separator ':' '$2 == "'"$full_version_variant"'" {print $3}' "$TOP_DIR/TAGS")
 
   ./tools/push.sh "$IMAGE:$full_version_variant"
-  for tag in ${tags[@]}; do
+  for tag in ${tags[*]}; do
     ./tools/push.sh "$IMAGE:$tag"
   done
 done
