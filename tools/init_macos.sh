085f2e196d79e45399b7e99a16c637e7c4dbc9b5
Chris Adams <chris@improbable.org>
2022-01-04 04:36:20 -0500

    Mac toolchain updates (#406)
    * Build (MacOS): Document the need for gnu-sed 
    This is required by the init_macos.sh script which had a separate error message in case this is missing.
    * init_macos.sh: support Homebrew prefixes
    
    This supports the now-standard /opt/homebrew prefix without breaking backwards compatibility on installs which predate that switch (most Intel Macs, for example).

--- tools/init_macos.sh
+++ tools/init_macos.sh
@@ -1,13 +1,26 @@
 # Source this file to prepare a mac for running scripts
-hash greadlink 2>/dev/null || { echo >&2 "Required tool greadlink is not installed, please run 'brew install coreutils'"; exit 1; }
-hash gfind 2>/dev/null || { echo >&2 "Required tool gfind is not installed, please run 'brew install findutils'"; exit 1; }
-hash gsed 2>/dev/null || { echo >&2 "Required tool gsed is not installed, please run 'brew install gnu-sed'"; exit 1; }
+hash greadlink 2>/dev/null || {
+  echo >&2 "Required tool greadlink is not installed, please run 'brew install coreutils'"
+  exit 1
+}
+hash gfind 2>/dev/null || {
+  echo >&2 "Required tool gfind is not installed, please run 'brew install findutils'"
+  exit 1
+}
+hash gsed 2>/dev/null || {
+  echo >&2 "Required tool gsed is not installed, please run 'brew install gnu-sed'"
+  exit 1
+}
+
+TOOL_PREFIX="$(brew --prefix)/bin"
+
 if [[ ! -d /tmp/docker-solr-bin ]]; then
   mkdir -p /tmp/docker-solr-bin >/dev/null 2>&1
-  ln -s /usr/local/bin/greadlink /tmp/docker-solr-bin/readlink
-  ln -s /usr/local/bin/gfind /tmp/docker-solr-bin/find
-  ln -s /usr/local/bin/gsed /tmp/docker-solr-bin/sed
+  ln -s $TOOL_PREFIX/greadlink /tmp/docker-solr-bin/readlink
+  ln -s $TOOL_PREFIX/gfind /tmp/docker-solr-bin/find
+  ln -s $TOOL_PREFIX/gsed /tmp/docker-solr-bin/sed
 fi
+
 if [[ ! $PATH == *"docker-solr-bin"* ]]; then
   export PATH=/tmp/docker-solr-bin:$PATH
   echo "Configuring for macOS - adding /tmp/docker-solr-bin first in path"
